703050f16ddb5391b87f9aa1896bf75e53a75bda
family <2224168716@qq.com>
2018-04-23 06:46:36 +0800

    wx:for detail page1

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -25,7 +25,7 @@ Page({
     var postId = event.currentTarget.dataset.postid;
     // console.log("on post id is" + postId);
     wx.navigateTo({
-      url: 'post-detail/post-detail',
+      url: 'post-detail/post-detail?id='+postId,
     });
   },
 
